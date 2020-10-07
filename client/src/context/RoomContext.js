d07bae90ec6c5bc8459b0ffb81cdd324eca62101
Cullen Smith <cullensmi@gmail.com>
2020-10-07 15:52:44 -0400

    added several different global methods

--- client/src/context/RoomContext.js
+++ client/src/context/RoomContext.js
@@ -1,35 +1,56 @@
 // Imports
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 
 // Exporting and creating our Room Context
 export const RoomContext = createContext();
 
-// Exporting our context provider
 export default function RoomContextProvider(props) {
-    const makeRooms = (floors, rooms) => {
-        let totalRooms = [];
-        let subArray = [];
-            for (let i = 0; i < floors; i++) {
-               for (let j = 0; j < rooms; j++) {
-                   subArray.push({room: (i+1)*(100)+(j+1), renter: null, price: (50*(i+1) - 10) + (10*(j+1)) + 45 })
-                //    console.log(subArray.length)
-                   if (subArray.length === 4){
-                    //    console.log('test')
-                    totalRooms.push(subArray)
-                       subArray = [];
-                   }     
-               }
+function roomGen(floors, rooms) {
+    let totalRooms = [];
+    let subArray = [];
+        for (let i = 0; i < floors; i++) {
+            for (let j = 0; j < rooms; j++) {
+                subArray.push({room: (i+1)*(100)+(j+1), renter: null, price: (50*(i+1) - 10) + (10*(j+1)) + 45 })
+            //    console.log(subArray.length)
+                if (subArray.length === 4){
+                //    console.log('test')
+                totalRooms.push(subArray)
+                    subArray = [];
+                }     
             }
-        // console.log(totalRooms)
-        return totalRooms;
-    }
+        }
+    // console.log(totalRooms)
+    return totalRooms;
+}    
 
-    let totalRooms = makeRooms(4,4);
-    
-    const [motelRooms, setRooms] = useState(totalRooms);   
+let twoDArray = roomGen(4,4);
+let totalRooms = twoDArray.flat()
+   
+const savedRooms = JSON.parse(localStorage.getItem('Server'));
+
+const [motelRooms, setRooms] = useState(savedRooms || totalRooms);   
+
+const ShowAvailableRooms = () => {
+    let availableRooms = totalRooms.filter(motel => motel.room>200)
+    // console.log(availableRooms)
+    setRooms(availableRooms)
+}
+
+const ShowRentedRooms = () => {
+    let rentedRooms = totalRooms.filter(motel => motel.room < 200)
+    setRooms(rentedRooms)
+}
+
+const ShowAllRooms = () => {
+    setRooms(savedRooms)
+}
+
+useEffect(() => {
+    localStorage.setItem('Server', JSON.stringify(motelRooms))
+}, [motelRooms])
+
     return (
-      <RoomContext.Provider value={{motelRooms}}>
-        {console.log(motelRooms)}
+      <RoomContext.Provider value={{motelRooms, ShowAvailableRooms, ShowRentedRooms, ShowAllRooms}}>
           {props.children}
         </RoomContext.Provider>
     )
