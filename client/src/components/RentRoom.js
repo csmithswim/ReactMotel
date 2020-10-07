bec838c2b77e5ce996302b4e8a7e0baa0713d1f5
Cullen Smith <cullensmi@gmail.com>
2020-10-07 15:54:36 -0400

    added map to iterate divs

--- client/src/components/RentRoom.js
+++ client/src/components/RentRoom.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RoomContext } from '../context/RoomContext'
+
+//Import our motel rooms object
 
 export default function RentRoom() {
+//Have a function that changes the renter value to the person's name, this can be done with a prompt tied to a method
+
+const { motelRooms } = useContext(RoomContext);
+// console.log(motelRooms)
+
+let availableRooms = motelRooms.filter(motel => motel.renter===null)
+let renderOnline = availableRooms.map((i) => <li key={i}>{i.renter}</li>);
+// console.log(availableRooms)
     return (
         <div>
-            
+            {renderOnline}
         </div>
     )
 }
--- client/src/components/RoomList.js
+++ client/src/components/RoomList.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RoomContext } from '../context/RoomContext';
 
 export default function RoomList() {
+const { motelRooms } = useContext(RoomContext);
+
+//Flatten array with array.prototype
+// let flatMotelRooms = motelRooms.flat()
+// console.log(flatMotelRooms)
+
+//Make two functions that filter out rented and available rooms to show them with display
+
+//Make these into divs
     return (
         <div>
-            
+            <div className = "container">            
+            {motelRooms.map(({room, renter, price},i) => {
+
+                    return (
+                      
+                        <div className = "rooms" key = {i}>
+                         Room #{room} is available{renter} and costs ${price}                        
+                        </div>)
+                    }
+                )
+            }             
+            </div>            
         </div>
     )
 }
