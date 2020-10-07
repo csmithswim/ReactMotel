75b120792c394127b8f2b4eb4bfe8c9f9dc35639
Cullen Smith <cullensmi@gmail.com>
2020-10-07 15:53:18 -0400

    added buttons and methods to show rooms

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RoomContext } from '../context/RoomContext';
 
 export default function NavBar() {
+    const { motelRooms } = useContext(RoomContext)
+    const { ShowAvailableRooms } = useContext(RoomContext)
+    const { ShowRentedRooms } = useContext(RoomContext)
+    const { ShowAllRooms } = useContext(RoomContext)
+         
     return (
         <div>
+            <h1 style = {{textAlign: "center"}}>Welcome To The Motel Of Your Dreams</h1>
+            <button onClick = {ShowAllRooms}>Click To See All Rooms</button>
+            <button onClick={ShowAvailableRooms}>Click to See Available Rooms</button>
+            <button onClick={ShowRentedRooms}>Click To See Occupied Rooms</button>
             
         </div>
     )
