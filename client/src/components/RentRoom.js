291dafec8d413c53563d1d33e977b6d4f8568bfc
Cullen Smith <cullensmi@gmail.com>
2020-10-08 17:14:47 -0400

    added a method to filter and map the output of all rooms available to rent and add rent method

--- client/src/components/RentRoom.js
+++ client/src/components/RentRoom.js
@@ -5,22 +5,33 @@ import { RoomContext } from '../context/RoomContext'
 
 export default function RentRoom() {
 
-    const { motelRooms } = useContext(RoomContext);
+    const { motelRooms, setRooms} = useContext(RoomContext);
 
-    let topFloor = motelRooms.filter(motel => motel.renter ===null)
-    
-    console.log(topFloor)
-    
+    let available = motelRooms.filter(motel => motel.renter === null)
+
+    const CheckIn = (e) => {
+        if (prompt("Are you sure you want to book this room? Y/N") === "Y"){
+            let guest = prompt("Enter your name:")
+            // motelRooms[e.id].renter=guest
+            console.log(motelRooms)
+            console.log(motelRooms[e.target.id])
+            motelRooms[e.target.id].renter = guest;
+            setRooms(motelRooms)
+        }
+
+    }
+ 
         return (
             <div>
                 <h3>
-                    These Rooms Are Available To Rent
+                    These {available.length} Rooms Are Available To Rent
                 </h3>
                     <div>
                         {motelRooms.filter(motel => motel.renter === null).map(
-                            ({room, price}) => {
+                            ({room, price},i) => {
                                 return (
-                                <div>
+                                <div   id = {i} key={i}>
+                                <button id = {i} key={i} onClick={CheckIn}>Click To Check In</button>
                                 {room} Is Available And Costs {price} A Night.
                                 </div>
                                 )
