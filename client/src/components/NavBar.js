af250274bb9d3ebf216e6ba680f5c70040f00596
Cullen Smith <cullensmi@gmail.com>
2020-10-09 17:48:04 -0400

    added two buttons and ternary operators to toggle display of rent and return room components

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,19 +1,31 @@
 import React, { useContext } from 'react'
 import { RoomContext } from '../context/RoomContext';
+import RentRoom from './RentRoom'
+import ReturnRoom from './ReturnRoom'
 
 export default function NavBar() {
-    const { motelRooms } = useContext(RoomContext)
-    const { ShowAvailableRooms } = useContext(RoomContext)
-    const { ShowRentedRooms } = useContext(RoomContext)
-    const { ShowAllRooms } = useContext(RoomContext)
-         
+    const { motelRooms, displayA, setDisplayA, displayUnAvailable, setDisplayUnAvailable} = useContext(RoomContext)
+
+    let flatMotel = motelRooms.flat()
+    let available = flatMotel.filter(motel => motel.renter === null)
     return (
         <div>
-            <h1 style = {{textAlign: "center"}}>Welcome To The Motel Of Your Dreams</h1>
-            <button onClick = {ShowAllRooms}>Click To See All Rooms</button>
-            <button onClick={ShowAvailableRooms}>Click to See Available Rooms</button>
-            <button onClick={ShowRentedRooms}>Click To See Occupied Rooms</button>
-            
+            <h1 style = {{textAlign: "center"}}>Welcome To The Motel Of Your Dreams<br/>
+            There Are {available.length} Rooms To Rent
+            </h1>          
+            <button onClick={() => setDisplayA(!displayA)}>Click To Hide/Display Available Rooms</button>
+            { displayA ?
+                (<RentRoom /> ) : (null) }
+
+            <br/><br/><br/>
+                
+            <button onClick={() => setDisplayUnAvailable(!displayUnAvailable)}>Click To Hide/Display Available Rooms</button>
+            { displayUnAvailable ?
+            (<ReturnRoom />) : (null)}
         </div>
     )
 }
+
+{/* <button onClick = {ShowAllRooms}>Click To See All Rooms</button>
+<button onClick={ShowAvailableRooms}>Click to See Available Rooms</button> */}
+// {/* <button onClick={ShowRentedRooms}>Click To See Occupied Rooms</button> */}
