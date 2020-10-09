da7d2257a8af8769813be8118cd9eed6aaa9ee45
Cullen Smith <cullensmi@gmail.com>
2020-10-09 17:47:20 -0400

    nested rent room into navbar

--- client/src/components/RentRoom.js
+++ client/src/components/RentRoom.js
@@ -5,29 +5,30 @@ export default function RentRoom() {
 
 const { motelRooms, setRooms} = useContext(RoomContext);
 
-let available = motelRooms.filter(motel => motel.renter === null)
+let flatMotel = motelRooms.flat()
+
+let available = flatMotel.filter(motel => motel.renter === null)
 
 const CheckIn = (e) => {
     if (prompt("Are you sure you want to book this room? Y/N") === "Y"){
         let guest = prompt("Enter your name:")
-            motelRooms[e.target.id].renter = guest;
+            flatMotel[e.target.id].renter = guest;
                 setRooms([...motelRooms])
         }
     } 
-
     return (
             <div>
                 <h3>
                     These {available.length} Rooms Are Available To Rent
                 </h3>
                     <div>
-                        {motelRooms.map(
+                        {flatMotel.map(
                             ({room, renter,price},i) => {
                                 if (renter===null){
                                     return (
                                     <div   id = {i} key={i}>
                                     <button id = {i} key={i} onClick={CheckIn}>Click To Check In</button>
-                                    {room} Is Available And Costs {price} A Night.
+                                    {room} Is Available And Costs ${price} A Night.
                                     </div>
                                 )} else return null;
                             }
